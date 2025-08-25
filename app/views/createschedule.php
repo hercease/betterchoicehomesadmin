0815ab48483482d90e3965acaf61cfb36e9a4581
hercease <hercease001@gmail.com>
2025-08-25 15:03:27 +0100

    update

--- app/views/createschedule.php
+++ app/views/createschedule.php
@@ -178,7 +178,6 @@
                                                 <span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg></span>
                                                 <input class="form-control ps-0 pointer" name="daterange" id="litepickerRangePlugin" placeholder="Select date range...">
                                             </div>
-                                            
                                         </div>
                                         <div class="col-md-2">
                                             <button type="submit" class="btn btn-primary w-100 align-item-center">Generate</button>
